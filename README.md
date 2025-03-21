dbb7ef8fce5cfa7defb5b2404c2564720f36d81d
Sven Zhang <zhangchong@kooker.cn>
2025-03-21 15:35:26 +0800

    [README] 删除无用代码

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SpringCloudDemo
+# 个人学习项目
+
+ - oauth2
+    + 鉴权服务
+    + 资源服务
+    + 自定义鉴权模式
+    + 自定义Token生成方式
+    + 资源服务opaqueToken自省
+ - WebSocket、Spring batch最小化配置
+ - Apache camel组件示例
+
--- auth-server/src/main/java/com/sven/auth/conf/AuthorizationServerConfiguration.java
+++ auth-server/src/main/java/com/sven/auth/conf/AuthorizationServerConfiguration.java
@@ -5,61 +5,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.lob.DefaultLobHandler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.security.oauth2.core.OAuth2Token;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationProvider;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationConsentAuthenticationProvider;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
-import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2AccessTokenGenerator;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2RefreshTokenGenerator;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import com.sven.auth.convert.CustomOAuth2PasswordAuthorizationConvert;
 import com.sven.auth.convert.CustomOAuth2SmsAuthorizationConvert;
 import com.sven.auth.filter.ValidateCodeFilter;
-import com.sven.auth.handler.CustomAuthorizationResponseSuccessHandler;
 import com.sven.auth.provider.CustomDaoAuthenticationProvider;
 import com.sven.auth.provider.CustomOAuth2PasswordAuthorizationProvider;
 import com.sven.auth.provider.CustomOAuth2SmsAuthorizationProvider;
-import com.sven.auth.service.UserInfo;
 import com.sven.auth.token.CustomeOAuth2TokenCustomizer;
 import com.sven.auth.token.generator.CustomOAuth2AuthorizationCodeGenerator;
 import com.sven.auth.token.generator.CustomeOAuth2AccessTokenGenerator;
-import com.sven.auth.vo.CustomUserMixin;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.List;
-import java.util.UUID;
 
 @Configuration
 public class AuthorizationServerConfiguration {
@@ -144,28 +120,6 @@ public class AuthorizationServerConfiguration {
         return new JdbcRegisteredClientRepository(jdbcTemplate);
     } 
     
-//    @Bean
-//    public OAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate,
-//            RegisteredClientRepository registeredClientRepository) {
-//        JdbcOAuth2AuthorizationService service = new JdbcOAuth2AuthorizationService(jdbcTemplate,
-//                registeredClientRepository);
-//        JdbcOAuth2AuthorizationService.OAuth2AuthorizationRowMapper authorizationRowMapper = new JdbcOAuth2AuthorizationService.OAuth2AuthorizationRowMapper(
-//                registeredClientRepository);
-//        authorizationRowMapper.setLobHandler(new DefaultLobHandler());
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        ClassLoader classLoader = JdbcOAuth2AuthorizationService.class.getClassLoader();
-//        List<Module> securityModules = SecurityJackson2Modules.getModules(classLoader);
-//        objectMapper.registerModules(securityModules);
-//        objectMapper.registerModule(new OAuth2AuthorizationServerJackson2Module());
-//        objectMapper.addMixIn(UserInfo.class, CustomUserMixin.class);
-//        authorizationRowMapper.setObjectMapper(objectMapper);
-//
-//        service.setAuthorizationRowMapper(authorizationRowMapper);
-//        
-//        return service;
-//    }
-    
     @Bean
     public OAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate,
             RegisteredClientRepository registeredClientRepository) {
@@ -179,37 +133,6 @@ public class AuthorizationServerConfiguration {
         return new DelegatingOAuth2TokenGenerator(accessTokenGenerator, new OAuth2RefreshTokenGenerator());
     }
     
-//    @Bean
-//    public JWKSource<SecurityContext> jwkSource() {
-//        KeyPair keyPair = generateRsaKey();
-//        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-//        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-//        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-//                .privateKey(privateKey)
-//                .keyID(UUID.randomUUID().toString())
-//                .build();
-//        JWKSet jwkSet = new JWKSet(rsaKey);
-//        return new ImmutableJWKSet<>(jwkSet);
-//    }
-//    
-//    private static KeyPair generateRsaKey() {
-//        KeyPair keyPair;
-//        try {
-//            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-//            keyPairGenerator.initialize(2048);
-//            keyPair = keyPairGenerator.generateKeyPair();
-//        }
-//        catch (Exception ex) {
-//            throw new IllegalStateException(ex);
-//        }
-//        return keyPair;
-//    }
-    
-//    @Bean
-//    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-//        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-//    }
-//    
     @Bean 
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
