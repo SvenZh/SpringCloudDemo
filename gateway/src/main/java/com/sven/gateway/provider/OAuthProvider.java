0d23bd93303e0ad8214c5034ab8cdb1b6eb92626
Sven Zhang <zhangchong@kooker.cn>
2025-04-09 17:38:29 +0800

    [插件] 添加了checkstyle和jacoco插件

--- gateway/src/main/java/com/sven/gateway/provider/OAuthProvider.java
+++ gateway/src/main/java/com/sven/gateway/provider/OAuthProvider.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class OAuthProvider {
     public static final String TARGET = "/**";
+    
     public static final String REPLACEMENT = "";
+    
     public static final String AUTH_KEY = "Authorization";
+    
     private static List<String> defaultSkipUrl = new ArrayList<>();
+    
     static {
         defaultSkipUrl.add("/test/**");
     }
---
+++ gateway/src/main/java/com/sven/gateway/provider/RequestProvider.java
@@ -0,0 +1,21 @@
+package com.sven.gateway.provider;
+
+import java.net.URI;
+import java.util.LinkedHashSet;
+
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class RequestProvider {
+
+    public static String getOriginalRequestUrl(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        LinkedHashSet<URI> uris = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+        URI requestUri = uris.stream().findFirst().orElse(request.getURI());
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        return UriComponentsBuilder.fromPath(requestUri.getRawPath()).queryParams(queryParams).build().toUriString();
+    }
+}
